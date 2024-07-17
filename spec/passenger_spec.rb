2364499520a46c6362cf5e84a8fc887c9b42beb8
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-17 11:12:38 -0400

    Test: initialize

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,1 +1,15 @@
 require "./lib/passenger"
+
+RSpec.describe Passenger do
+    before(:each) do
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+    end
+
+    describe '#initialize' do
+        it 'can initialize' do
+            expect(charlie).to be_a_instance_of(Passenger)
+            expect(taylor).to be_a_instance_of(Passenger)
+        end
+    end
+end
