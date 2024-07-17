0227fe9e93179a339fb1c7329874339cf159c0cc
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-17 11:14:08 -0400

    Created a Passenger class

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,1 +1,10 @@
 require "./lib/passenger"
+
+RSpec.describe Passenger do 
+  before(:each) do 
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+  end
+
+  
+end
