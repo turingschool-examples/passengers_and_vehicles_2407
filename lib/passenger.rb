dda9493fb9ae23dbd3a0755c79ea4a268c9efa6f
Stefan Bloom <stefanjbloom88@gmail.com>
2024-07-17 10:33:30 -0600

    Test:basic passenger info test done

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -5,8 +5,8 @@ class Passenger
                 :age
 
     def initialize(passenger_info) 
-        @name = passenger_info[:name]
-        @age = passenger_info[:age]
+        @name = passenger_info[:name.to_s]
+        @age = passenger_info[:age.to_s]
     end
 
 
