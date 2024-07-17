f7a42f7522cfa1ba74594901bf1f966bc1324a12
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-07-17 11:46:20 -0600

    added an adult? method to the Passenger class since the test error said that method was undefined

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -5,4 +5,19 @@ class Passenger
         @name = attributes[:name]
         @age = attributes[:age]
     end
+
+    def adult?
+        if age == 18
+            puts true
+        else
+            puts false
+        end
+    end
+
+    require 'pry'; binding.pry
+    # charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    # taylor = Passenger.new({"name" => "Taylor", "age" => 12})
 end
+
+# charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+# taylor = Passenger.new({"name" => "Taylor", "age" => 12})
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -5,45 +5,37 @@ RSpec.describe do
 
     end
 
-    describe 'name' do
-        it 'has a name' do
-            charlie = Passenger.new({"name" => "Charlie", "age" => 18})
-            expect(charlie.name).to eq("Charlie")
-        end
+    it 'exists' do
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
     end
 
-    describe 'age' do
-        it 'has an age' do
-            charlie = Passenger.new({"name" => "Charlie", "age" => 18})
-            expect(charlie.age).to eq(18)
-        end
+    it 'has a name' do
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        expect(charlie.name).to eq("Charlie")
     end
 
-    describe 'adult' do
-        it 'might be an adult' do
-            charlie = Passenger.new({"name" => "Charlie", "age" => 18})
-            expect(charlie.adult?).to eq(true)
-        end
+    it 'has an age' do
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        expect(charlie.age).to eq(18)
     end
 
-    describe 'name' do
-        it 'has a name' do
-            taylor = Passenger.new({"name" => "Taylor", "age" => 12})
-            expect(taylor.name).to eq("Taylor")
-        end
+    it 'might be an adult' do
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        expect(charlie.adult?).to eq(true)
     end
 
-    describe 'age' do
-        it 'has an age' do
-            taylor = Passenger.new({"name" => "Taylor", "age" => 12})
-            expect(taylor.age).to eq(12)
-        end
+    it 'has a name' do
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+        expect(taylor.name).to eq("Taylor")
     end
 
-    describe 'adult' do
-        it 'might be an adult' do
-            taylor = Passenger.new({"name" => "Taylor", "age" => 12})
-            expect(taylor.adult?).to eq(false)
-        end
+    it 'has an age' do
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+        expect(taylor.age).to eq(12)
+    end
+
+    it 'might be an adult' do
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+        expect(taylor.adult?).to eq(false)
     end
 end
