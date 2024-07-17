fd8a87d385ad116fb6dbb3567ddd56354f8e2c0d
Bryan Willett <btwillett2003@gmail.com>
2024-07-17 10:46:06 -0500

    Added adult method to check age.

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,4 +1,19 @@
 class Passenger
-    def initialize()
+    attr_reader :name
+                :age
+                :adult
+
+    def initialize(name, age, adult)
+        @name = name
+        @age = age
+        @adult = adult
+    end
+
+    def adult?
+        if
+            @age >= 18
+            true
+        else
+            false
     end
 end
