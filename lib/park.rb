2fc44017e450acd4c49dec60ddce0a5cf1b3e51c
John Hill <jphill1989@gmail.com>
2024-07-17 10:29:41 -0600

    Created new add_vehicle method for, and its respective test and it's passing

--- lib/park.rb
+++ lib/park.rb
@@ -1,10 +1,15 @@
 class Park
 
-  attr_reader :name, :price
+  attr_reader :name, :price, :vehicles
 
   def initialize(name, price)
     @name = name
     @price = price
+    @vehicles = []
+  end
+
+  def add_vehicle(vehicle)
+    @vehicles << vehicle
   end
 
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Park do
   before(:each) do
     @park_1 = Park.new("Zion", 10)
     @vehicle_1 = Vehicle.new("2001", "Honda", "Civic")
+    @vehicle_2 = Vehicle.new("2013", "Chevrolet", "Cruze")
     @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
     @taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
     @jude = Passenger.new({"name" => "Jude", "age" => 20})       
@@ -17,9 +18,19 @@ RSpec.describe Park do
       expect(@park_1).to be_an_instance_of(Park)
       expect(@park_1.name).to eq("Zion")
       expect(@park_1.price).to eq(10)
+      expect(@park_1.vehicles).to eq([])
 
     end
 
   end
+  describe "#add_vehicle" do
+    it "can list all the vehicles that entered the park" do
+      @park_1.add_vehicle(@vehicle_1)
+      @park_1.add_vehicle(@vehicle_2)
+
+      expect(@park_1.vehicles).to eq([@vehicle_1, @vehicle_2])
+    end
+  end
+
 
 end
