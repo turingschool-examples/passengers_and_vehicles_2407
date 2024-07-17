73de458d7cac129a003b7a5716a05e3c1fb6a40d
Shane Galvin <Sgalvin36@gmail.com>
2024-07-17 09:34:46 -0600

    Add psudotests for vehicle class

---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,39 @@
+require "./lib/vehicle"
+
+RSpec.describe Vehicle do
+    before(:each) do
+        vehicle = Vehicle.new('2001', 'Honda', 'Civic')
+    end
+
+    describe "#initialize" do
+        it 'exists' do
+        end
+
+        it 'has access to all elements on initialize' do
+        end
+    end
+
+    describe "#speeding" do
+        it 'has speed attribute' do
+        end
+
+        it 'can change speeding attribute' do
+        end
+    end
+
+    describe "#passengers" do
+        before(:each) do
+            charlie = Passenger.new({"name" => "Charlie", "age" => 18})  
+            jude = Passenger.new({"name" => "Jude", "age" => 20})  
+            taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+        end
+        
+        it 'has a list of passengers' do
+        end
+
+        it 'can add passengers to the list' do
+        end
+
+        it 'can track the number of adults' do
+        end
+    end
