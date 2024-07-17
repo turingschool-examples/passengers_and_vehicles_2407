e326263d90d954335997ef6c00029ff8162b75a6
Stefan Bloom <stefanjbloom88@gmail.com>
2024-07-17 11:10:51 -0600

    Feat:vehicle exists and intitalizes

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe '#Passenger' do
     @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
     @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
     end
+    
     it 'exists' do
         expect(@charlie).to be_an_instance_of(Passenger)
         expect(@taylor).to be_an_instance_of(Passenger)
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -7,7 +7,15 @@ RSpec.configure do |config|
     end
 
 RSpec.describe '#Vehicle' do
-    before(:each) do
+    it 'exists' do
+        @vehicle = Vehicle.new("2001", "Honda", "Civic") 
+        expect(@vehicle).to be_an_instance_of(Vehicle)
+    end
 
+    it 'can initialize' do
+        @vehicle = Vehicle.new("2001", "Honda", "Civic")
+        expect(@vehicle.year).to eq("2001")
+        expect(@vehicle.make).to eq("Honda")
+        expect(@vehicle.model).to eq("Civic")
     end
 end
