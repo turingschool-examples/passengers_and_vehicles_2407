5a699a3e35b5527f6a6a7dc8ba166be0d93a9ad7
Kyle Delaney <khdelaney99@gmail.com>
2024-07-17 11:14:28 -0400

    set up structure for iteration 1 tests

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,1 +1,24 @@
 require "./lib/passenger"
+
+RSpec.configure do |config|
+    config.formatter = :documentation
+end
+
+Rspec.describe 'Passenger' do
+
+    describe '#initialize' do
+
+    end
+
+    describe '#adult?' do 
+
+    end
+
+    describe '#driver?' do
+
+    end
+
+    describe '#drive' do
+    
+    end
+end
