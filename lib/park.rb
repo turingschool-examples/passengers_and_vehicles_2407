407ec9019853d59ecd7ddac850baddec133cdc9a
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-17 12:25:05 -0400

    Feat: vehicles_entered method

--- lib/park.rb
+++ lib/park.rb
@@ -1,10 +1,14 @@
 class Park
     
-    attr_reader :name, :admission
+    attr_reader :name, :admission, :vehicles_in_park
 
     def initialize(park_info)
         @name = park_info[:name]
         @admission = park_info[:price]
+        @vehicles_in_park = []
     end
 
+    def vehicles_entered(vehicle)
+        @vehicles_in_park << vehicle
+    end
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Park do
     before(:each) do
         @flatwoods = Park.new({name: "Flatwoods", price: 2})
         @hillsborough = Park.new({name: "Hillsborough River", price: 4})
+        @vehicle = Vehicle.new("2001", "Honda", "Civic")
+        @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        @jude = Passenger.new({"name" => "Jude", "age" => 20})
+        @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
     end
 
     describe '#initialize' do
@@ -19,6 +23,15 @@ RSpec.describe Park do
 
             expect(@hillsborough.name).to eq("Hillsborough River")
             expect(@hillsborough.admission).to eq(4)
+        end
+    end
+
+    describe '#vehicles_entered' do
+        it 'lists all vehicles that entered the park' do
+            expect(@flatwoods.vehicles_in_park).to eq([])
+
+            @flatwoods.vehicles_entered(@vehicle)
+            expect(@flatwoods.vehicles_in_park).to eq([@vehicle])
         end
     end
 end
