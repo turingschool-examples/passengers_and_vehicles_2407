35d5822cd8d21bf4d30f25941ac438580f3df9a4
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-17 12:33:44 -0400

    Test: number of passengers in park

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -6,10 +6,16 @@ RSpec.describe Park do
     before(:each) do
         @flatwoods = Park.new({name: "Flatwoods", price: 2})
         @hillsborough = Park.new({name: "Hillsborough River", price: 4})
+        
         @vehicle = Vehicle.new("2001", "Honda", "Civic")
+        
         @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
         @jude = Passenger.new({"name" => "Jude", "age" => 20})
         @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+        @vehicle.add_passenger(@charlie)
+        @vehicle.add_passenger(@jude)
+        @vehicle.add_passenger(@taylor)
     end
 
     describe '#initialize' do
@@ -32,6 +38,11 @@ RSpec.describe Park do
 
             @flatwoods.vehicle_enters(@vehicle)
             expect(@flatwoods.vehicles_in_park).to eq([@vehicle])
+        end
+        it 'lists all passengers that entered the park' do
+            # require 'pry'; binding.pry
+            @flatwoods.vehicle_enters(@vehicle)
+            expect(@flatwoods.vehicles_in_park[0].passengers.count).to eq(3)
         end
     end
 end
