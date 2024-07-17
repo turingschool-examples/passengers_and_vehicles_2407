51dd25c75be9ac5e52e2d4441f85db8074eec5cc
Shane Galvin <Sgalvin36@gmail.com>
2024-07-17 09:12:42 -0600

    Add describe blocks for each feature

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,1 +1,19 @@
 require "./lib/passenger"
+
+Rspec.new Passenger do
+    describe '#initialize' do
+        before(:each) do
+            Passenger.new({"name" => "Charlie", "age" => 18})
+        end
+
+        it 'exists' do
+            expect(passenger).to be_an_instance Passenger
+        end
+    end
+
+    describe '#adult?' do
+    end
+
+    describe '#driver' do
+    end
+end
