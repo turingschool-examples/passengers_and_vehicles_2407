631d8569393b317e1e09674063f93e6ab509f8c1
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-17 10:03:14 -0600

    test: can add passengers

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -21,4 +21,16 @@ RSpec.describe Vehicle do
     it 'can contain passengers' do
         expect(@vehicle.passengers).to eq ([])
     end
+
+    it 'can add passengers' do
+        @charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+        @jude = Passenger.new({"name" => "Jude", "age" => 20})
+        @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+        @vehicle.add_passenger(@charlie)
+        @vehicle.add_passenger(@jude)
+        @vehicle.add_passenger(@taylor)
+
+        expect(@vehicle.passengers.length).to eq(3)
+    end
 end
