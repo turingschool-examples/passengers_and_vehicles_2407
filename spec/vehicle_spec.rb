a8b71d4ca132ffaf98a1757a133cf0ae84210245
Shawn Thompson <thompson_shawn@hotmail.com>
2024-07-17 12:28:49 -0500

    ignore

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -23,5 +23,21 @@ RSpec.describe Vehicle do
         expect(@vehicle.speeding?).to eq(true)
     end
 
+    it 'add passengers' do
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        jude = Passenger.new({"name" => "Jude", "age" => 20})
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+        expect(@vehicle.passengers?).to eq([])
+
+        # vehicle.add_passenger(charlie) 
+        # vehicle.add_passenger(jude) 
+        # vehicle.add_passenger(taylor) 
+
+
+        # expect(@vehicle.passengers).to eq("Charlie", "Jude", "Taylor")
+    end
+
+
 
 end
