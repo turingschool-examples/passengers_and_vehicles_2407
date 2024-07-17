fdbd8ab3704af4a0183c053c9359134d8ff07ffc
BrodyNics77 <brodynichols777@gmail.com>
2024-07-17 12:52:43 -0500

    finishing up park class

---
+++ lib/park.rb
@@ -0,0 +1,28 @@
+class Park 
+    attr_reader :name :address :phone, :serivces
+    
+    def initialze(name, address, :phone)
+        @name = name
+        @address = address
+        @phone = phone
+        @serivces = serivces
+    end
+
+    def add_service(Park)
+        @serivces << serivce
+    end
+
+    def admission_price(vehicle)
+    when @park_services
+        vehicle.charge(50)
+        end
+    end
+
+    def current_vehicles
+        
+    end
+end
+
+#needs more functionality 
+#needs more statments
+#totally stumped atm 
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -2,7 +2,7 @@ class Vehicle
     attr_reader :vehicle_make, :vehicle_model:, speeding
 
     def initialze(vehicle_make, vehicle_model, speeding)
-        @vehicle_make= vehicle_make
+        @vehicle_make = vehicle_make
         @vehicle_model = vehicle_model
         @speeding = speeding
     end
