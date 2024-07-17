7f724ed4ab16d17fdfd1cf3cdc9079e067351f6a
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-17 10:08:43 -0600

    test: knows ammount of adults in vehicle

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -32,5 +32,18 @@ RSpec.describe Vehicle do
         @vehicle.add_passenger(@taylor)
 
         expect(@vehicle.passengers.length).to eq(3)
+        expect(@vehicle.passengers).to eq([@charlie, @jude, @taylor])
+    end
+
+    it 'knows ammount of adults in vehicle' do
+        @charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+        @jude = Passenger.new({"name" => "Jude", "age" => 20})
+        @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+        @vehicle.add_passenger(@charlie)
+        @vehicle.add_passenger(@jude)
+        @vehicle.add_passenger(@taylor)
+
+        expect(@vehicle.num_adults).to eq(2)
     end
 end
