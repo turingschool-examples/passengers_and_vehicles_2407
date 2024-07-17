b587e5a156688415c746e1486cc31e07b51f4aa1
Joel Davalos <jsdavalos@gmail.com>
2024-07-17 09:22:58 -0600

    Inital commit

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,1 +1,28 @@
 require "./lib/passenger"
+
+RSpec.describe Passenger do
+  before(:each) do 
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+  end
+
+  it 'exists' do 
+
+  end
+
+  it 'has attributes' do
+    # name and age
+  end
+
+  it 'can tell if passenger is an adult' do
+
+  end
+
+  it 'can tell if passenger is driver' do
+
+  end
+
+  
+
+
+end
