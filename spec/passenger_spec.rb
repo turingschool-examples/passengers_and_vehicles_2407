95d06adeb14b7823e5ca6cba0a5b1ba3a99c7c23
John Hill <jphill1989@gmail.com>
2024-07-17 09:12:09 -0600

    set up the structure for the passenger test. Still need to code Passenger class

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,1 +1,18 @@
 require "./lib/passenger"
+
+
+RSpec.describe Passenger do
+  before(:each) do
+    @passenger = Passenger.new
+  end
+
+  describe "#initialize" do
+    it "can initialize" do
+      expect()
+    end
+
+  end
+
+
+
+end
