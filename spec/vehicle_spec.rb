6192ff65a8c44e877cca868fc37c8f52c3fef46c
Kyle Delaney <khdelaney99@gmail.com>
2024-07-17 11:53:35 -0400

    set up structure of test for Vehicle class

---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,32 @@
+require "./lib/vehicle"
+require 'rspec'
+
+RSpec.configure do |config|
+    config.formatter = :documentation
+end
+
+RSpec.describe 'Vehicle' do
+    describe '#initialize' do
+
+    end
+
+    describe '#speeding?' do
+
+    end
+
+    describe '#speed' do
+
+    end
+
+    describe '#add_passenger()' do
+
+    end
+    
+    describe '#passengers' do
+
+    end
+    
+    describe '#num_adults' do
+
+    end
+end
