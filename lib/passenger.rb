8d2fd2d9029afdb95c1dec66d80ff6741c337dba
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-07-17 09:33:02 -0600

    added passenger tests for name, age, and adult status, created basic structure for the passenger class

---
+++ lib/passenger.rb
@@ -0,0 +1,7 @@
+class Passenger
+    attr_reader :name, :age
+
+    def initialize(name, age)
+        
+    end
+end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,1 +1,33 @@
 require "./lib/passenger"
+
+Rspec.describe do
+    before(:all) do
+
+    end
+
+    describe 'name' do
+        it 'has a name' do
+            charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+            taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+            expect(passenger_1.name).to eq("Charlie")
+            expect(passenger_2.name).to eq("Taylor")
+        end
+    
+    describe 'age' do
+        it 'has an age' do
+            charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+            taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+            expect(passenger_1.age).to eq(18)
+            expect(passenger_2.age).to eq(12)
+        end
+    end
+
+    describe 'adult' do
+        it 'is an adult' do
+            charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+            taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+            expect(passenger_1.adult?).to eq(true)
+            expect(passenger_2.adult?).to eq(false)
+        end
+    end
+end
