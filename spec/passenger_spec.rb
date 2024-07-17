c0ba7a7424340e5d16fc32e53cb5d742497d95f1
Jim Macur <jim.macur@gmail.com>
2024-07-17 09:17:56 -0600

    creates initialize test

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,1 +1,12 @@
 require "./lib/passenger"
+
+RSpec.describe Passenger do
+
+
+    describe '#initialize' do
+        it 'exists' do
+            passenger = Passenger.new
+            expect(passenger).to be_an_instance_of(Passenger)
+        end
+    end
+end
