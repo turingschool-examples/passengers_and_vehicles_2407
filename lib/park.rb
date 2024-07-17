817bdbb1ebf7dc85886e444fade4a186fd413a01
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-17 13:14:06 -0400

    Created method for vehicles and add_vehicles

--- lib/park.rb
+++ lib/park.rb
@@ -1,8 +1,13 @@
 class Park 
-attr_reader :name, :admission_price
+attr_reader :name, :admission_price, :vehicles
 
   def initialize(name, admission_price)
     @name = name
     @admission_price = admission_price
+    @vehicles = []
+  end
+
+  def add_vehicles(vehicles)
+    @vehicles << vehicles
   end
 end
