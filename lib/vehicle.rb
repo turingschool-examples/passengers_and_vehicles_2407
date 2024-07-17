ea03458255b3c4c68d9ca186c45f5809a72cff0c
Kyle Delaney <khdelaney99@gmail.com>
2024-07-17 12:09:38 -0400

    pass test for #speeding? method in vehicle

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -3,11 +3,17 @@ require "./lib/passenger"
 class Vehicle
     attr_reader :year,
                 :make,
-                :model
+                :model,
+                :speeding
 
     def initialize(year, make, model)
         @year = year
         @make = make
         @model = model
+        @speeding = false
+    end
+
+    def speeding?
+        @speeding
     end
 end
