ee1a1d958711526765129153b2451e02cb43a493
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-17 10:31:52 -0600

    test: can initialize new park object

---
+++ spec/park_spec.rb
@@ -0,0 +1,14 @@
+require './lib/vehicle'
+require './lib/passenger'
+require './lib/park'
+
+RSpec.describe Park do
+    before(:each) do
+        park1 = Park.new("Yosemite", 60)
+        park2 = Park.new("Glacier", 50)
+    end
+
+    it 'exists' do
+        expect(park1).to be_instance_of(Park)
+    end
+end
