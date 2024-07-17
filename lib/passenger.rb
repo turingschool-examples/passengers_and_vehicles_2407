97e5c113055306f87591e100b673cddf33bdc9a8
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-17 09:19:19 -0600

    test: can initialize passenger object

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,4 +1,8 @@
 class Passenger
+    attr_reader :name, :age
 
-    
+    def initialize(passenger_details)
+        @name = passenger_details[:name]
+        @age = passenger_details[:age]
+    end
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,7 +1,13 @@
 require "./lib/passenger"
 
 RSpec.describe Passenger do
-   
+    before(:each) do
+        @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    end
+
+    it 'exists' do
+        expect(@charlie).to be_instance_of(Passenger)
+    end 
 
 
 end
