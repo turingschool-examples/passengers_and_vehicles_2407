02fc27946bc18b89a8861a3c586501de0427e582
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-17 09:59:02 -0600

    feat: passenger attr

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,11 +1,12 @@
 class Vehicle
-    attr_reader :year, :make, :model
+    attr_reader :year, :make, :model, :speeding, :passengers
 
     def initialize(year, make, model, speeding = false)
         @year = year
         @make = make
         @model = model
         @speeding = speeding
+        @passengers = []
     end
 
     def speeding?
