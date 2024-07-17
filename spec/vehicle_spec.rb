bb11314a7e054925fba4a0877a29865434e20414
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-17 12:35:43 -0400

    Added test for num _adults

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -45,6 +45,18 @@ RSpec.describe Vehicle do
         expect(@vehicle.passengers).to include(charlie, jude, taylor)
       end
     end
+
+    describe 'number of adults' do 
+      it 'returns only adult?' do 
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        jude = Passenger.new({"name" => "Jude", "age" => 20})    
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+        @vehicle.add_passenger(charlie)
+        @vehicle.add_passenger(jude)
+        @vehicle.add_passenger(taylor)
+        expect(@vehicle.num_adults).to eq(2)
+      end
+    end
   end
   
 end
