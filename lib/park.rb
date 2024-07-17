23f3d644000f0e997f8c8a64ef628a01d2442226
Candice Cirbo <thecirbos@icloud.com>
2024-07-17 11:57:11 -0600

    Adds final method to park

--- lib/park.rb
+++ lib/park.rb
@@ -12,4 +12,25 @@ class Park
     def enter_park(vehicle)
         @vehicles << vehicle
     end
+
+    def vehicle_passengers
+        @vehicles.flat_map do |vehicle|
+          vehicle.passengers.map do |passenger|
+            passenger.name
+          end
+        end
+    end
+   
+
+    def potential_revenue
+        total_charge = 0
+         @vehicles.each do |vehicle|
+            vehicle.passengers.each do |passenger|
+            if passenger.age >= 18  
+                total_charge += @price 
+            end
+         end
+       end
+       total_charge
+   end
 end
