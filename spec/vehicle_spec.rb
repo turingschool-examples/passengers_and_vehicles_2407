71ab1a501c3861eae0d2b3533481c8a78aea9431
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-17 09:51:20 -0600

    test: can initialize new vehicle object

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -2,5 +2,11 @@ require './lib/vehicle'
 require './lib/passenger'
 
 RSpec.describe Vehicle do
-    
+    before(:each) do
+        @vehicle = Vehicle.new("2001", "Honda", "Civic")
+    end
+
+    it 'exists' do
+        expect(@vehicle).to be_instance_of(Vehicle)
+    end
 end
