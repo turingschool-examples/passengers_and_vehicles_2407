c04a94dfc96a210b09454838a82569f2072cdc05
BrodyNics77 <brodynichols777@gmail.com>
2024-07-17 10:41:04 -0500

    added test and functions

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,1 +1,13 @@
 require "./lib/passenger"
+
+Rspec.describe Passenger do
+    before(:each) do
+        Passenger.new({name: 'Charlie', age: '18', adult: 'false'})
+        Passenger.new({name: 'Taylore', age: '12', adult: 'false'})
+    end
+    require 'pry'; binding.pry
+
+
+
+
+#Passenger.new({"name" => "Charlie", "age" => 18}) Passenger.new({"name" => "Taylor", "age" => 12})    
