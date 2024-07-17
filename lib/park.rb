19316673891c258868313abcf688e95ac2069896
Jim Macur <jim.macur@gmail.com>
2024-07-17 12:00:14 -0600

    adds list minors to park class

--- lib/park.rb
+++ lib/park.rb
@@ -30,4 +30,12 @@ class Park
         adults = all_passengers.select { |passenger| passenger.adult?}
         adults_in_order = adults.sort_by { |adult| adult.name } 
     end
+    
+    def list_minors
+        all_passengers = @vehicles.map { |vehicle| vehicle.passengers }
+        all_passengers = all_passengers.flatten
+    #    list all that are not minors
+        minors = all_passengers.select { |passenger| !passenger.adult?}
+        minors_in_order = minors.sort_by { |adult| adult.name } 
+    end
 end
