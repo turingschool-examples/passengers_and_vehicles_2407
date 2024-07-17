d2b2ec32bd931432c16bd5f7a147aed49e4f3010
Shawn Thompson <thompson_shawn@hotmail.com>
2024-07-17 10:23:05 -0500

    Initial commit

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,1 +1,13 @@
 require "./lib/passenger"
+
+RSpec.describe Passenger do
+    it 'creates vehicles from the WA EV registrations data' do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      expect(charlie.name).to eq('charlie') 
+      expect(charlie.age).to eq(18) 
+      
+    end
+  
+  end
