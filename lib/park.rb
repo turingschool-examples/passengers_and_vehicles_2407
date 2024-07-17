3eef3bd92c2be355b934a80589dd63cb175dcf6c
Jim Macur <jim.macur@gmail.com>
2024-07-17 10:37:41 -0600

    adds empty array for vehicles in Park

--- lib/park.rb
+++ lib/park.rb
@@ -1,8 +1,9 @@
 class Park
-    attr_reader :name, :admission
+    attr_reader :name, :admission, :vehicles
 
     def initialize(name, admission)
         @name = name
         @admission = admission
+        @vehicles = []
     end
 end
