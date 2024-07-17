7617febf467280473aec3f6de23ff97b22750c8d
Bryan Willett <btwillett2003@gmail.com>
2024-07-17 10:35:24 -0500

    Added passengers to spec file.

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,4 +1,9 @@
 require "./lib/passenger"
-require 'rspec'
 
-RSpec.describe 
+RSpec.describe Passenger do
+    before(:each) do
+        @passenger_1 = Passenger.new('Charlie', 18)
+        @passenger_2 = Passenger.new('Taylor', 12)
+    end
+    
+end
