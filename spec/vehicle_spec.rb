60a7e04e46fb05cb0163deb778b7dc3364111d1e
Kyle Delaney <khdelaney99@gmail.com>
2024-07-17 12:02:37 -0400

    add tests for #initialize method in Vehicle

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -6,8 +6,28 @@ RSpec.configure do |config|
 end
 
 RSpec.describe 'Vehicle' do
-    describe '#initialize' do
+    before(:each) do
+        @vehicle = Vehicle.new("2001", "Honda", "Civic")
+        @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        @jude = Passenger.new({"name" => "Jude", "age" => 20})  
+        @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
 
+    end
+
+    describe '#initialize' do
+        it 'will initialize an instance of the Car class' do
+            expect(@vehicle).to be_an_instance_of Vehicle
+        end
+
+        it 'will hold a year attribute based on the argument' do
+            expect(@vehicle.year).to eq "2001"
+        end
+        it 'will hold a make attribute based on the argument' do
+            expect(@vehicle.make).to eq "Honda"
+        end
+        it 'will hold a model attribute based on the argument' do
+            expect(@vehicle.model).to eq "Civic"
+        end
     end
 
     describe '#speeding?' do
