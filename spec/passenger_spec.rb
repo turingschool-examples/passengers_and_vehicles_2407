17c515fd367d79e8e76ac4de30d70f2cd2ea84f2
Shawn Thompson <thompson_shawn@hotmail.com>
2024-07-17 11:16:52 -0500

    feat: adult?

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,13 +1,29 @@
 require "./lib/passenger"
 
 RSpec.describe Passenger do
-    it 'creates vehicles from the WA EV registrations data' do
-      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
-      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
-
-      expect(charlie.name).to eq('charlie') 
-      expect(charlie.age).to eq(18) 
-      
+    before(:each) do
+        @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
     end
-  
+
+    describe '#initialize' do
+        it 'initializes a passenger' do
+        expect(@charlie).to be_an_instance_of(Passenger)
+        end
+    end
+
+    it 'creates passenger' do
+        expect(@charlie.name).to eq('Charlie') 
+        expect(@charlie.age).to eq(18) 
+    end
+
+    it 'asks if it is an adult?' do
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+        expect(@charlie.adult?).to eq(true)
+        expect(@taylor.adult?).to eq(false)
+    end
+
+
   end
