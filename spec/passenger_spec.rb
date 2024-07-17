e1321a531b806d43d81254529ee319e6c04b881e
Jim Macur <jim.macur@gmail.com>
2024-07-17 09:21:20 -0600

    Adds a before :each

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,12 +1,17 @@
 require "./lib/passenger"
 
 RSpec.describe Passenger do
-
+    before(:each) do
+        @passenger = Passenger.new({"name" => "Charlie", "age" => 18}) 
+    end
 
     describe '#initialize' do
         it 'exists' do
-            passenger = Passenger.new
-            expect(passenger).to be_an_instance_of(Passenger)
+            expect(@passenger).to be_an_instance_of(Passenger)
+        end
+
+        it'has parameters' do
+
         end
     end
 end
