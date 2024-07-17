cafc86ab84fe294fb601f79a5390263543d724dd
Kyle Delaney <khdelaney99@gmail.com>
2024-07-17 13:08:15 -0400

    add #initialize method test for park

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -7,28 +7,33 @@ end
 
 RSpec.describe 'Park' do
     before(:each) do
-    
+        @park = Park.new("Arlong Park", "$25")
+        @vehicle = Vehicle.new("2001", "Honda", "Civic")
+        @zoro = Passenger.new({"name" => "Zoro", "age" => 19})
+        @nami = Passenger.new({"name" => "Nami", "age" => 20})  
+        @chopper = Passenger.new({"name" => "Chopper", "age" => 15})
+        @luffy = Passenger.new({"name" => "Luffy", "age" => 17})
     end
 
     describe '#initialize()' do
-        it 'will be an isntance of a park object' do
-
+        it 'will be an instance of a park object' do
+            expect(@park).to be_an_instance_of Park
         end
         
         it 'will hold a name attribute based on the argument' do
-
+            expect(@park.name).to eq "Arlong Park"
         end
 
         it 'will hold an admission price attribute based on the argument' do
-
+            expect(@park.admission_price).to eq "$25"
         end
 
         it 'will hold a vehicles argument that is initialized as an empty array' do
-
+            expect(@park.vehicles).to eq {}
         end
 
         it 'will hold a passengers argument that is initialized as an empty array' do
-
+            expect(@park.passengers).to eq {}
         end
     end
 
@@ -38,7 +43,7 @@ RSpec.describe 'Park' do
         end
 
         it 'creates the revenue figure by charging the admission price to every adult included in the passenger list' do
-            
+
         end
     end
 end
