9eca3161e5cdee8acd7351f7afd3af120a387ba2
Stefan Bloom <stefanjbloom88@gmail.com>
2024-07-17 09:15:02 -0600

    Test: passenger class exists

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,1 +1,11 @@
 require "./lib/passenger"
+
+RSpec.configure do |config|
+    config.formatter = :documentation
+    end
+
+RSpec.describe '#passenger' do
+    it 'exists' do
+        passenger = Passenger.new
+    end
+end
