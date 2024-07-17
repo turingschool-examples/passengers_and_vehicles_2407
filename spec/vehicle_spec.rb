4e08e9fa9ec95c8099626997ffb3e1775cf41207
Jim Macur <jim.macur@gmail.com>
2024-07-17 09:59:32 -0600

    add attributes test

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -10,5 +10,11 @@ RSpec.describe Vehicle do
         it 'can exists' do
             expect(@vehicle).to be_an_instance_of(Vehicle)
         end
+
+        it 'has attributes' do
+            expect(@vehicle.year).to eq "2001"
+            expect(@vehicle.make).to eq "Honda"
+            expect(@vehicle.model).to eq "Civic"
+        end
     end
 end
